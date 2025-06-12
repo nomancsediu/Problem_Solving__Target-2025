73183345a56211bb570e0b8551572cacbc893081
Abdullah Al Noman <abd.noman720@gmail.com>
2025-06-12 21:40:32 +0600

    abd

---
+++ Codeforces/800/dr_dc.py
@@ -0,0 +1,16 @@
+t = int (input())
+
+for i in range(t):
+    n = int(input())
+    s = input().strip()
+
+    cnt = s.count('1')
+    ans = 0
+
+    for c in s:
+        if c == '1':
+            ans=cnt-1
+        else:
+            ans=cnt+1
+    
+    print(cnt)
